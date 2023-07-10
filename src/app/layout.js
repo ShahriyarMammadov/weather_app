24ff8cae0ac0438ed4640e3ba1bf2f8cbf1b70b3
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-07-10 22:50:12 +0400

    mobile brauzer color

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <meta name="theme-color" content="#80231c" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
